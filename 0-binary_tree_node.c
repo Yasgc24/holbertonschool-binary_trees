fdb55f0ad264ddbecbaad8b6002709911ed0b9ef
Yasgc24 <4537@holbertonschool.com>
2022-07-26 14:36:18 -0500

    Modified files betty style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,12 +9,10 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new = (binary_tree_t*)malloc(sizeof(binary_tree_t));
+	binary_tree_t *new = (binary_tree_t*)malloc(sizeof(binary_tree_t));
 
-    if (new == NULL)
-    {
-        return (NULL);
-    }
-
-
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 }
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,5 +9,5 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    
+
 }
