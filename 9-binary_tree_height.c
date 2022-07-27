bd3cd6d5acd0e99dbe04ebb13542020e3ab22bd4
Yasgc24 <4537@holbertonschool.com>
2022-07-27 14:20:00 -0500

    Add task 9 solution

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,5 +8,18 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    
+	int height_l = 0, height_r = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	height_l = binary_tree_height(tree->left);
+	height_l = binary_tree_height(tree->right);
+
+	if (height_l >= height_r)
+		return (height_l + 1);
+	return (height_r + 1);
 }
