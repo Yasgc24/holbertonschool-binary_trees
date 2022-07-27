561243e41c284dd8eb7173826e1409639d4deaea
edwardzuniga <4593@holbertonschool.com>
2022-07-27 14:09:52 -0500

    Post-order traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -9,9 +9,10 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
+	if (tree == NULL || func == NULL)
 		return;
-	func(tree->n);
+
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
