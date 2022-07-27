442e62429d4947d266d630eaf97ad9f3e4303b92
Yasgc24 <4537@holbertonschool.com>
2022-07-27 15:08:42 -0500

    Add task 13 solution

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,5 +8,16 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    
+	size_t count = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	else if (tree->left || tree->right)
+	{
+		count = 1;
+		count += binary_tree_nodes(tree->left);
+		count += binary_tree_nodes(tree->right);
+	}
+	return (count);
 }
