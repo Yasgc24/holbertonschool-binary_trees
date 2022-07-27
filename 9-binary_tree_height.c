c73e994cbba29810c92340f7a0f703738a1bc139
Yasgc24 <4537@holbertonschool.com>
2022-07-27 14:22:42 -0500

    Modified task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,7 +17,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	height_l = binary_tree_height(tree->left);
-	height_l = binary_tree_height(tree->right);
+	height_r = binary_tree_height(tree->right);
 
 	if (height_l >= height_r)
 		return (height_l + 1);
