a3786ef157444ee614a9920203bc69dab446d971
edwardzuniga <4593@holbertonschool.com>
2022-07-27 13:17:22 -0500

    Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -30,7 +30,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	{
 		new->left = parent->left;
-		parent->left->parent = new;
+		new->left->parent = new;
 	}
 
 	parent->left = new;
